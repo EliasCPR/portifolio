e8db76432f5c1edd502d36b22838dbbddd055ee7
Elias Oliveira <elias.oliveira@adviseu.com.br>
2021-12-23 15:46:54 -0300

    refatoring

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
 
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-
-}
 
 window.onscroll = () =>{
 
@@ -20,13 +15,3 @@ window.onscroll = () =>{
   }
 
 }
-
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut();
